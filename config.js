d815c29c7bb2ec18b6c0ebc41b00083530c78f3f
amagistrale <69706385+amagistrale@users.noreply.github.com>
2024-12-04 12:43:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {
     "website" : "https://amagistrale.github.io/FPPReportingMapTest/", //Change to the URL that displays the thank you page. This link is inactive as of Oct 15. https://amagistrale.github.io/FPPReportingMap/
-    "script" : "https://script.google.com/macros/s/AKfycbw1gX038umY60MWeO8K02Fx4QKiIqrzc8yLYe3ZMcF-e1TaPEspx3Y_gCI8loBPKRPK/exec" //Change to coincide with your own appscript. 
+    "script" : "https://script.google.com/macros/s/AKfycbx51eNtdQe1d9rAxE9xjfzAh8Pp2j1gk-XUnKGcV6rTlAnS6TxJafgz0uGIDzEe8_I/exec" //Change to coincide with your own appscript. 
 }
