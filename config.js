aacd1e6d624289e1ba8f932b11cb1821f63c391d
amagistrale <69706385+amagistrale@users.noreply.github.com>
2024-12-04 12:12:56 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+    "website" : "Reporting Map/index.html", //Change to the URL that displays the thank you page. This link is inactive as of Oct 15. https://amagistrale.github.io/FPPReportingMap/
+    "script" : "https://script.google.com/macros/s/AKfycbw1gX038umY60MWeO8K02Fx4QKiIqrzc8yLYe3ZMcF-e1TaPEspx3Y_gCI8loBPKRPK/exec" //Change to coincide with your own appscript. 
+}
